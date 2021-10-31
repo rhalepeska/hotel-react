4d4e8cbb27fd1d2a74568416bff0b97499e8bbca
rhalepeska <rhalepeska@skillstorm.com>
2021-10-31 16:09:14 -0400

    route check

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          auto deploy test 2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <h1>Home Page</h1>
+        </Route>
+        <Route path="/about" exact>
+          <h1>About Page</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
