c86468f323aede6df947b7b076f600e24376efdb
rhalepeska <rhalepeska@skillstorm.com>
2021-11-01 12:08:05 -0400

    fixed env and urls

--- src/utils/user-API.jsx
+++ src/utils/user-API.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 
-const baseURL = process.env.REACT_APP_BASE_URL || `https://hotel-express.azurewebsites.net/api`;
-
+const baseURL = process.env.REACT_APP_BASE_URL || `http://localhost:8080/api/`
+ 
 const api = axios.create({
     baseURL
 })
