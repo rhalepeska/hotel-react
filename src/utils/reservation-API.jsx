48d4ff404fe92e54fe2342d517ad49593d226d05
rhalepeska <rhalepeska@skillstorm.com>
2021-11-03 18:29:18 -0400

    pull from previous repo

--- src/utils/reservation-API.jsx
+++ src/utils/reservation-API.jsx
@@ -7,12 +7,20 @@ const api = axios.create({
     baseURL
 })
 
+export const getAllReservation = async (id) => {
+    return await api.get(`/reservation/`);
+};
+
 export const getReservationById = async (id) => {
     return await api.get(`/reservation/${id}`);
 };
 
 export const getReservationsByDay = async (checkin, checkout, roomId) => {
     return await api.get(`/reservation/day/${checkin}/${checkout}/${roomId}`);
+};
+
+export const updateReservationById = async (reservationId, userId, roomPrice, inputReservationDate) => {
+    return await api.put(`/reservation/update/${reservationId}/${userId}/${roomPrice}`, inputReservationDate);
 };
 
 export const createReservation = async (reservationData, userId, roomPrice) => {
--- src/utils/user-API.jsx
+++ src/utils/user-API.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const baseURL = process.env.REACT_APP_BASE_URL || `http://localhost:8080/api/`
- 
+
 const api = axios.create({
     baseURL
 })
